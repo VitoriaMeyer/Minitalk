7c433c4c02864cec136e5601c65cea31ce94973e
VitoriaMeyer <vitoria.mey.s@gmail.com>
2023-01-17 12:35:28 +0000

    17/01

---
+++ client.c
@@ -0,0 +1,37 @@
+#include "minitalk.h"
+
+void	send_bits(unsigned int c, int server_pid)
+{
+	int				i;
+	unsigned char	bit;
+
+	i = 8;
+	while (i > 0)
+	{
+		bit = (unsigned char)(c & 1);
+		c = *((unsigned char *) &c) >> 1;
+		if (bit)
+			kill(server_pid, SIGUSR1);
+		else
+			kill(server_pid, SIGUSR2);
+		i--;
+		usleep(350);
+	}
+}
+
+int main(int agrc, char **argv)
+{
+    int pid;
+	int	i;
+
+	i = 0;
+    if (agrc != 3)
+        ft_putstr_fd("Invalid argument, provide only PID and message\n", 1);
+    pid = ft_atoi(argv[1]);
+	while (argv[2])
+	{
+		send_bits(argv[i], pid);
+		i++;
+	}
+	return (0);
+}
---
+++ server.c
@@ -0,0 +1,48 @@
+#include "minitalk.h"
+
+char	ft_power(unsigned char base, unsigned char power)
+{
+	unsigned char	sum;
+
+	sum = 1;
+	while (power)
+	{
+		sum = sum * base;
+		power--;
+	}
+	return (sum);
+}
+
+void	read_signals(int signal)
+{
+	static unsigned int		c;
+	static int				counter;
+
+	if (signal == SIGUSR1)
+		c += ft_power(2, counter);
+	counter++;
+	if (counter == 8)
+	{
+		counter = 0;
+		write(1, &c, 1);
+		c = 0;
+	}
+	return ;
+}
+
+int main()
+{  
+    pid_t	id;
+    struct sigaction    sigact;
+
+	id = getpid();
+    ft_putnbr_fd(id, 1);
+	ft_putchar_fd('\n', 1);
+    sigact.sa_flags = SA_RESTART;
+	sigact.sa_handler = read_signals;
+    sigaction(SIGUSR1, &sigact, NULL);
+	sigaction(SIGUSR2, &sigact, NULL);
+    while (1)
+        pause();
+    return (0);
+}
